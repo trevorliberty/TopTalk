ac4ae75fce3c3dda4088997e9acb426dd1fbda8e
David Likhovodov <likhovodovd@gmail.com>
2021-04-17 20:11:48 -0700

    Progress on client-server interaction

--- src/index.js
+++ src/index.js
@@ -1,40 +1,196 @@
+/**
+ * Modules
+ */
 const express = require('express');
 const path = require('path');
 const socketio = require('socket.io');
 const http = require('http');
+const uuid = require('uuid');
+const run = require('./api/dataprocess');
 
+/**
+ * Classes
+ */
+class TopicComments {
+	constructor() {
+		this.comments = new Map();
+	}
+}
+
+class Comment {
+	constructor(authorName, content, articleId, replyingToId) {
+		this.authorName = authorName;
+		this.content = content;
+		this.articleId = articleId;
+		this.replyingToId = replyingToId;
+		this.responses = new Map();
+		this.score = 0;
+	}
+
+	/**
+	 * 
+	 * @param {Map[string, Comment]} messageMap 
+	 * @returns This map as json encoded string
+	 */
+	static mapToJson(messageMap) {
+		let ret = Object.create(null);
+		console.log(ret)
+		for (const [k, v] of messageMap) {
+			ret[k] = v;
+		}
+		return ret;
+	}
+
+	toJSON() {
+		return {
+			authorName: this.authorName,
+			content: this.content,
+			articleId: this.articleId,
+			replyingToId: this.replyingToId,
+			score: this.score,
+			responses: Comment.mapToJson(this.responses)
+		}
+	}
+
+}
+
+
+/**
+ * Variables
+ */
+// Exteranal vars
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-const run = require('./api/dataprocess');
-const INIT = 'init';
-// let objects = run();
 
-let objects = require('./results');
+// Our vars
+// let topicArticleData = run();
+const topicArticleData = require('./results');
+const users = new Map(); // Map[userId, userName]
+const topicIdToTopic = new Map() // Map[topicId, Map[messageId, Comment]]
+topicArticleData.forEach(topic => topicIdToTopic.set(topic.id, new TopicComments()))
+
+// Status constants
+const STATUS_REJECTED = 'STATUS_REJECTED';
+const STATUS_ACCEPETED = 'STATUS_ACCEPETED';
+
+// Client side events
+const CLIENT_EVENT_REGISTER = 'CLIENT_EVENT_REGISTER';
+const CLIENT_EVENT_PICK_TOPIC = 'CLIENT_EVENT_PICK_TOPIC';
+const CLIENT_EVENT_COMMENT = 'CLIENT_EVENT_COMMENT';
+const CLIENT_EVENT_UPVOTE = 'CLIENT_EVENT_UPVOTE';
+const CLIENT_EVENT_DOWNVOTE = 'CLIENT_EVENT_DOWNVOTE';
+
+// Server side evetns
+const SERVER_EVENT_COMMENT = 'SERVER_EVENT_COMMENT';
+const SERVER_EVENT_UPVOTE = 'SERVER_EVENT_UPVOTE';
+const SERVER_EVENT_DOWNVOTE = 'SERVER_EVENT_DOWNVOTE';
+
+
+/**
+ * Configurations
+ */
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
 
+
+/**
+ * Routes
+ */
 app.get('/', (req, res) => {
 	res.render('index');
 });
 
-io.on('connection', (socket) => {
-	let topicInFocus = null;
-	//TODO handle user connects
-	socket.on('disconnect', () => {
-		//TODO handle user disconnects
-	});
 
-	socket.on('roomSelect', (roomId) => {
-		if (topicInFocus) {
-			//unsubscribe UUID
-		} else {
-			socket.join(roomId);
+/**
+ * Socket
+ */
+io.on('connection', (socket) => {
+
+	// User-sepecific variables
+	let topicInFocusId = null;
+	let userName = "";
+	this.upvotedCommentIds = new Set();
+	this.downvotedCommentIds = new Set();
+
+
+	socket.on('disconnect', () => {
+		if (users.has(socket.id)) {
+			users.delete(socket.id);
 		}
 	});
-	socket.emit(INIT, objects);
+
+	// User registers with a username
+	socket.on(CLIENT_EVENT_REGISTER, (requestedName, callback) => {
+		let callbackStatus = '';
+
+		if (Array.from(users.values()).includes(requestedName)) {
+			callbackStatus = STATUS_REJECTED;
+		} else {
+			this.userName = requestedName
+			users.set(socket.id, requestedName);
+			callbackStatus = STATUS_ACCEPETED;
+		}
+
+		callback({
+			status: callbackStatus,
+		});
+	});
+
+	// User puts topic in focus
+	socket.on(CLIENT_EVENT_PICK_TOPIC, (topicId, callback) => {
+		if (topicInFocusId) {
+			socket.leave(topicInFocusId);
+		}
+		topicInFocusId = topicId
+		socket.join(topicInFocusId);
+
+		callback({
+			topicComments: Comment.mapToJson(topicIdToTopic.get(topicInFocusId).comments),
+			upvotedCommentIds: this.upvotedCommentIds,
+			downvotedCommentIds: this.downvotedCommentIds
+		})
+	});
+
+	// User makes a comment
+	socket.on(CLIENT_EVENT_COMMENT, (content, articleId, replyingToId) => {
+		let newCommentId = uuid.v4();
+		topicInFocusComments = topicIdToTopic.get(topicInFocusId).comments
+
+		if (replyingToId == null) {
+			topicInFocusComments.set(newCommentId, new Comment(userName, content, articleId, replyingToId))
+		} else {
+			topicInFocusComments.get(replyingToId).responses.set(newCommentId, new Comment(userName, content, articleId, replyingToId))
+		}
+		socket.to(topicInFocusId).emit(SERVER_EVENT_COMMENT, senderId, newCommentId, userName, content, articleId, replyingToId);
+	});
+
+	// User upvotes a comment
+	// Assumes client enforces user not bieng able to upvote their own comment
+	socket.on(CLIENT_EVENT_UPVOTE, (commentId) => {
+		const topicInFocus = topicIdToTopic.get(topicInFocusId)
+		if (!topicInFocus.upvotedCommentIds.has(commentId)) {
+			topicInFocus.downvotedCommentIds.delete(commentId);
+			topicInFocus.upvotedCommentIds.add(commentId)
+			topicInFocus.socket.to(topicInFocusId).emit(SERVER_EVENT_UPVOTE, commentId);
+		}
+	});
+
+	// User upvotes a comment
+	// Assumes client enforces user not bieng able to upvote their own comment
+	socket.on(CLIENT_EVENT_DOWNVOTE, (commentId) => {
+		const topicInFocus = topicIdToTopic.get(topicInFocusId)
+		if (!topicInFocus.downvotedCommentIds.has(commentId)) {
+			topicInFocus.upvotedCommentIds.delete(commentId);
+			topicInFocus.downvotedCommentIds.add(commentId)
+			topicInFocus.socket.to(topicInFocusId).emit(SERVER_EVENT_DOWNVOTE, commentId);
+		}
+	});
 });
 
+/**
+ * Server
+ */
 server.listen(process.env.PORT, () => {
 	console.log(`listening on port ${process.env.PORT}`);
 });
--- src/public/index.js
+++ src/public/index.js
@@ -1,1 +1,65 @@
-//socket client, jqeuery
+const socket = io();
+
+// Status constants
+const STATUS_REJECTED = 'STATUS_REJECTED';
+const STATUS_ACCEPETED = 'STATUS_ACCEPETED';
+
+// Client side events
+const CLIENT_EVENT_REGISTER = 'CLIENT_EVENT_REGISTER';
+const CLIENT_EVENT_PICK_TOPIC = 'CLIENT_EVENT_PICK_TOPIC';
+const CLIENT_EVENT_COMMENT = 'CLIENT_EVENT_COMMENT';
+const CLIENT_EVENT_UPVOTE = 'CLIENT_EVENT_UPVOTE';
+const CLIENT_EVENT_DOWNVOTE = 'CLIENT_EVENT_DOWNVOTE';
+
+// Server side evetns
+const SERVER_EVENT_COMMENT =  'SERVER_EVENT_COMMENT';
+const SERVER_EVENT_UPVOTE = 'SERVER_EVENT_UPVOTE';
+const SERVER_EVENT_DOWNVOTE = 'SERVER_EVENT_DOWNVOTE';
+
+
+function register(userName) {
+
+    socket.emit(CLIENT_EVENT_REGISTER, userName, (response) => {
+        if (response.status === STATUS_ACCEPETED) {
+            //TODO
+        } else {
+            // TODO
+        }
+    });
+}
+
+function focusTopic(topicId) {
+    socket.emit(CLIENT_EVENT_PICK_TOPIC, topicId, (response) => {
+        console.log(response.topicMessages) // TODO
+        console.log(response.upvotedCommentIds)
+        console.log(response.downvotedCommentIds)
+    });
+}
+
+function message(content, articleId, replyingToId) {
+    socket.emit(CLIENT_EVENT_COMMENT, content, articleId, replyingToId)
+}
+
+function upvote(commentId) {
+    socket.emit(CLIENT_EVENT_UPVOTE, commentId)
+}
+
+function downvote(commentId) {
+    socket.emit(CLIENT_EVENT_DOWNVOTE, commentId)
+}
+
+socket.on(SERVER_EVENT_COMMENT, (senderId, messageId, content, articleId, replyingToId) => {
+    if(replyingToId) {
+        //TODO handle if response message
+    } else {
+        //TODO handle original comment
+    }
+})
+
+socket.on(SERVER_EVENT_UPVOTE, (commentId) => {
+    // TODO
+})
+
+socket.on(SERVER_EVENT_DOWNVOTE, (commentId) => {
+    // TODO
+})
