6810501e429bd8c27b88c657b23c3c63acdd6763
Joe Leland <jml27@pdx.edu>
2021-04-18 09:17:58 -0700

    merge conflicts

--- src/public/index.js
+++ src/public/index.js
@@ -1,163 +1,161 @@
 const socket = io();
 
 function htmlDecode(value) {
-	return $('<textarea/>').html(value).text();
+  return $("<textarea/>").html(value).text();
 }
 /**
  * Objects
  */
 
 class Article {
-	// constructor(sourceId, sourceName, authorName, title, description, url, urlToImage, content) {
-	constructor(sourceObject) {
-		this.id = sourceObject.id;
-		this.htmlBody = sourceObject.htmlBody;
-		this.sourceId = sourceObject.source.id;
-		this.sourceName = sourceObject.source.name;
-		this.author = sourceObject.name;
-		this.title = sourceObject.title;
-		this.description = sourceObject.description;
-		this.url = sourceObject.url;
-		this.urlToImage = sourceObject.urlToImage;
-		this.publishedAt = sourceObject.publishedAt;
-		this.content = sourceObject.content;
-		this.weight = sourceObject.wieght;
-	}
+  // constructor(sourceId, sourceName, authorName, title, description, url, urlToImage, content) {
+  constructor(sourceObject) {
+    this.id = sourceObject.id;
+    this.htmlBody = sourceObject.htmlBody;
+    this.sourceId = sourceObject.source.id;
+    this.sourceName = sourceObject.source.name;
+    this.author = sourceObject.name;
+    this.title = sourceObject.title;
+    this.description = sourceObject.description;
+    this.url = sourceObject.url;
+    this.urlToImage = sourceObject.urlToImage;
+    this.publishedAt = sourceObject.publishedAt;
+    this.content = sourceObject.content;
+    this.weight = sourceObject.wieght;
+  }
 }
 
 class Topic {
-	constructor(sourceObject) {
-		this.id = sourceObject.id;
-		this.sourceArticle = new Article(JSON.parse(sourceObject.source));
-		this.relatedArticles = [];
+  constructor(sourceObject) {
+    this.id = sourceObject.id;
+    this.sourceArticle = new Article(JSON.parse(sourceObject.source));
+    this.relatedArticles = [];
 
-		JSON.parse(sourceObject.relatedArticles).forEach((relatedArticle) =>
-			this.relatedArticles.push(new Article(relatedArticle)),
-		);
+    JSON.parse(sourceObject.relatedArticles).forEach((relatedArticle) =>
+      this.relatedArticles.push(new Article(relatedArticle))
+    );
 
-		this.comments = new Map();
+    this.comments = new Map();
 
-		let parsedComments = JSON.parse(sourceObject.comments)
-		for(const key of Object.keys(parsedComments)) {
-			this.comments.set(key, new Comment(parsedComments[key]));
-		}
+    let parsedComments = JSON.parse(sourceObject.comments);
+    for (const key of Object.keys(parsedComments)) {
+      this.comments.set(key, new Comment(parsedComments[key]));
+    }
 
-		// for (let k of Object.keys(sourceObject.comments)) {
-		// 	this.comments.set(k, new Comment(sourceObject.comments[k]));
-		// }
-
-	}
-	toJSON() {
-		return {
-			id: this.id,
-			source: JSON.parse(this.sourceArticle),
-			relatedArticles: JSON.stringify(this.relatedArticles),
-			comments: Comment.mapToJson(this.comments),
-		};
-	}
+    // for (let k of Object.keys(sourceObject.comments)) {
+    // 	this.comments.set(k, new Comment(sourceObject.comments[k]));
+    // }
+  }
+  toJSON() {
+    return {
+      id: this.id,
+      source: JSON.parse(this.sourceArticle),
+      relatedArticles: JSON.stringify(this.relatedArticles),
+      comments: Comment.mapToJson(this.comments),
+    };
+  }
 }
 
 class Comment {
-	constructor(sourceObject) {
-		this.authorName = sourceObject.authorName;
-		this.content = sourceObject.content;
-		this.articleId = sourceObject.articleId;
-		this.replyingToId = sourceObject.replyingToId;
-		this.responses = new Map();
-		if (sourceObject.responses) {
-			for (let k of Object.keys(sourceObject.responses)) {
-				this.responses.set(k, new Comment(sourceObject.responses[k]));
-			}
-		}
-		this.score = sourceObject.score;
-	}
+  constructor(sourceObject) {
+    this.authorName = sourceObject.authorName;
+    this.content = sourceObject.content;
+    this.articleId = sourceObject.articleId;
+    this.replyingToId = sourceObject.replyingToId;
+    this.responses = new Map();
+    if (sourceObject.responses) {
+      for (let k of Object.keys(sourceObject.responses)) {
+        this.responses.set(k, new Comment(sourceObject.responses[k]));
+      }
+    }
+    this.score = sourceObject.score;
+  }
 
-	static mapToJson(messageMap) {
-		let ret = Object.create(null);
-		for (const [k, v] of messageMap) {
-			ret[k] = v;
-		}
-		return ret;
-	}
+  static mapToJson(messageMap) {
+    let ret = Object.create(null);
+    for (const [k, v] of messageMap) {
+      ret[k] = v;
+    }
+    return ret;
+  }
 
-	toJSON() {
-		return {
-			authorName: this.authorName,
-			content: this.content,
-			articleId: this.articleId,
-			replyingToId: this.replyingToId,
-			responses: Comment.mapToJson(this.responses),
-			score: this.score,
-		};
-	}
+  toJSON() {
+    return {
+      authorName: this.authorName,
+      content: this.content,
+      articleId: this.articleId,
+      replyingToId: this.replyingToId,
+      responses: Comment.mapToJson(this.responses),
+      score: this.score,
+    };
+  }
 }
 
 // Status constants
-const STATUS_REJECTED = 'STATUS_REJECTED';
-const STATUS_ACCEPETED = 'STATUS_ACCEPETED';
+const STATUS_REJECTED = "STATUS_REJECTED";
+const STATUS_ACCEPETED = "STATUS_ACCEPETED";
 
 // Client side events
-const CLIENT_EVENT_REGISTER = 'CLIENT_EVENT_REGISTER';
-const CLIENT_EVENT_GET_TOPIC = 'CLIENT_EVENT_GET_TOPIC';
-const CLIENT_EVENT_COMMENT = 'CLIENT_EVENT_COMMENT';
-const CLIENT_EVENT_UPVOTE = 'CLIENT_EVENT_UPVOTE';
-const CLIENT_EVENT_DOWNVOTE = 'CLIENT_EVENT_DOWNVOTE';
+const CLIENT_EVENT_REGISTER = "CLIENT_EVENT_REGISTER";
+const CLIENT_EVENT_GET_TOPIC = "CLIENT_EVENT_GET_TOPIC";
+const CLIENT_EVENT_COMMENT = "CLIENT_EVENT_COMMENT";
+const CLIENT_EVENT_UPVOTE = "CLIENT_EVENT_UPVOTE";
+const CLIENT_EVENT_DOWNVOTE = "CLIENT_EVENT_DOWNVOTE";
 
 // Server side evetns
-const SERVER_EVENT_COMMENT = 'SERVER_EVENT_COMMENT';
-const SERVER_EVENT_UPVOTE = 'SERVER_EVENT_UPVOTE';
-const SERVER_EVENT_DOWNVOTE = 'SERVER_EVENT_DOWNVOTE';
+const SERVER_EVENT_COMMENT = "SERVER_EVENT_COMMENT";
+const SERVER_EVENT_UPVOTE = "SERVER_EVENT_UPVOTE";
+const SERVER_EVENT_DOWNVOTE = "SERVER_EVENT_DOWNVOTE";
 
 function register(userName) {
-	socket.emit(CLIENT_EVENT_REGISTER, userName, (response) => {
-		if (response.status === STATUS_ACCEPETED) {
-			//TODO
-		} else {
-			// TODO
-		}
-	});
+  socket.emit(CLIENT_EVENT_REGISTER, userName, (response) => {
+    if (response.status === STATUS_ACCEPETED) {
+      //TODO
+    } else {
+      // TODO
+    }
+  });
 }
 
-
-function handleServerSideComments(topic){
-	for(const [k,comment] of topic.comments.entries()){
-		console.log(comment);
-	}
+function handleServerSideComments(topic) {
+  for (const [k, comment] of topic.comments.entries()) {
+    console.log(comment);
+  }
 }
 
 function focusTopic(topicId) {
-	socket.emit(CLIENT_EVENT_GET_TOPIC, topicId, (response) => {
-		const topic = new Topic(JSON.parse(response.topic));
-		handleServerSideComments(topic)
-		const upvotedCommentIds = JSON.parse(response.upvotedCommentIds);
-		const downvotedCommentIds = JSON.parse(response.downvotedCommentIds);
-		const topicHTML = response.topicHTML;
-		handleFocus(topicHTML, topic, upvotedCommentIds, downvotedCommentIds);
-	});
+  socket.emit(CLIENT_EVENT_GET_TOPIC, topicId, (response) => {
+    const topic = new Topic(JSON.parse(response.topic));
+    handleServerSideComments(topic);
+    const upvotedCommentIds = JSON.parse(response.upvotedCommentIds);
+    const downvotedCommentIds = JSON.parse(response.downvotedCommentIds);
+    const topicHTML = response.topicHTML;
+    handleFocus(topicHTML, topic, upvotedCommentIds, downvotedCommentIds);
+  });
 }
 
 function message(content, articleId, replyingToId) {
-	socket.emit(
-		CLIENT_EVENT_COMMENT,
-		content,
-		articleId,
-		replyingToId,
-		(response) => {
-			return response.id;
-		}
-	);
+  socket.emit(
+    CLIENT_EVENT_COMMENT,
+    content,
+    articleId,
+    replyingToId,
+    (response) => {
+      return response.id;
+    }
+  );
 }
 
 function upvote(commentId) {
-	socket.emit(CLIENT_EVENT_UPVOTE, commentId);
+  socket.emit(CLIENT_EVENT_UPVOTE, commentId);
 }
 
 function downvote(commentId) {
-	socket.emit(CLIENT_EVENT_DOWNVOTE, commentId);
+  socket.emit(CLIENT_EVENT_DOWNVOTE, commentId);
 }
 
-function getCommentHTML(senderId,messageId,content,articleId,replyingToId){
-	return `
+function getCommentHTML(senderId, messageId, content, articleId, replyingToId) {
+  return `
 		<div class="card">
 		<div class="card-header">${senderId}</div>
 		<div class="card-body">
@@ -168,83 +166,101 @@ function getCommentHTML(senderId,messageId,content,articleId,replyingToId){
 		</div>
 	`;
 }
-function handleCommentEmission(senderId,messageId,content,articleId,replyingToId){
-	let html = getCommentHTML(senderId, messageId,content,articleId,replyingToId)
-	$(`#messageArea_${articleId}`).append(html);
+function handleCommentEmission(
+  senderId,
+  messageId,
+  content,
+  articleId,
+  replyingToId
+) {
+  let html = getCommentHTML(
+    senderId,
+    messageId,
+    content,
+    articleId,
+    replyingToId
+  );
+  $(`#messageArea_${articleId}`).append(html);
+  $(`#messageArea_${articleId}`).scrollTop(
+    $(`#messageArea_${articleId}`)[0].scrollHeight
+  );
 }
-
 
 function handleFocus(topicHTML, topic, upvotedCommentIds, downvotedCommentIds) {
-	$('#topicInFocus').html(topicHTML);
-	$('#sidebarCollapse_').on('click', function () {
-		$('#content').width('70vw');
-		$('#sidebar').toggleClass('active');
-		$('#sidebarCollapse_').css('display', 'none');
-	});
+  $("#topicInFocus").html(topicHTML);
+  $("#sidebarCollapse_").on("click", function () {
+    $("#content").width("70vw");
+    $("#sidebar").toggleClass("active");
+    $("#sidebarCollapse_").css("display", "none");
+  });
 
-	$('[id^="show_"]').click(function (e) {
-		// do something
-		let doc = $(this)[0].id.replace('show', '#article');
-		$('#active_article').html(htmlDecode($(doc)[0].innerHTML));
-	});
+  $('[id^="show_"]').click(function (e) {
+    // do something
+    let doc = $(this)[0].id.replace("show", "#article");
+    $("#active_article").html(htmlDecode($(doc)[0].innerHTML));
+  });
 
-	$('.commentPicker').keydown((e)=>{
-		if(e.keyCode === 13){
-			e.preventDefault();
-			let id = e.currentTarget.id.replace('comment_', '');
-			let comment = e.currentTarget.value;
-			message(comment, id, null)
-			e.currentTarget.value = ''
-		}
-	})
+  $(".commentPicker").keydown((e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      let id = e.currentTarget.id.replace("comment_", "");
+      let comment = e.currentTarget.value;
+      message(comment, id, null);
+      e.currentTarget.value = "";
+    }
+  });
 }
 $(document).ready(() => {
-	socket.on(
-		SERVER_EVENT_COMMENT,
-		(senderId, messageId, content, articleId, replyingToId) => {
-			if (replyingToId) {
-				//TODO handle if response message
-			} else {
-				//TODO handle original comment
-			}
-			handleCommentEmission(senderId,messageId,content,articleId,replyingToId)
+  socket.on(
+    SERVER_EVENT_COMMENT,
+    (senderId, messageId, content, articleId, replyingToId) => {
+      if (replyingToId) {
+        //TODO handle if response message
+      } else {
+        //TODO handle original comment
+      }
+      handleCommentEmission(
+        senderId,
+        messageId,
+        content,
+        articleId,
+        replyingToId
+      );
+    }
+  );
 
+  socket.on(SERVER_EVENT_UPVOTE, (commentId) => {
+    // TODO
+  });
 
-		},
-	);
+  socket.on(SERVER_EVENT_DOWNVOTE, (commentId) => {
+    // TODO
+  });
 
-	socket.on(SERVER_EVENT_UPVOTE, (commentId) => {
-		// TODO
-	});
+  $("#sidebarCollapse").on("click", function () {
+    $("#content").width("100%");
+    // $('#sidebar').toggleClass('active');
+    $("#sidebar").css("margin-left", "-30vw");
 
-	socket.on(SERVER_EVENT_DOWNVOTE, (commentId) => {
-		// TODO
-	});
+    $("#sidebarCollapse_").css("display", "block");
+    setTimeout(() => {
+      $("#sidebarCollapse_").css("display", "block");
+    }, 130);
+  });
+  $("#sidebarCollapse_").on("click", function () {
+    $("#content").width("70vw");
+    // $('#sidebar').toggleClass('active');
+    $("#sidebarCollapse_").css("display", "none");
+    $("#sidebar").css("margin-left", 0);
+  });
 
-	$('#sidebarCollapse').on('click', function () {
-		$('#content').width('100%');
-		// $('#sidebar').toggleClass('active');
-		$('#sidebar').css('margin-left','-30vw')
+  $('[id^="show_"]').click(function (e) {
+    // do something
+    let doc = $(this)[0].id.replace("show", "#article");
+    $("#active_article").html(htmlDecode($(doc)[0].innerHTML));
+  });
 
-			$('#sidebarCollapse_').css('display', 'block');
-		setTimeout(() => {
-			$('#sidebarCollapse_').css('display', 'block');
-		}, 130);
-	});
-	$('#sidebarCollapse_').on('click', function () {
-		$('#content').width('70vw');
-		// $('#sidebar').toggleClass('active');
-		 $('#sidebarCollapse_').css('display', 'none');
-		 $('#sidebar').css('margin-left', 0)
-	});
-
-	$('[id^="show_"]').click(function (e) {
-		// do something
-		let doc = $(this)[0].id.replace('show', '#article');
-		$('#active_article').html(htmlDecode($(doc)[0].innerHTML));
-	});
-
-	$('.clickCatcher').click((e) => {
-		focusTopic(e.currentTarget.id);
-	});
+  $(".clickCatcher").click((e) => {
+    focusTopic(e.currentTarget.id);
+  });
 });
