fcd6dace0865d5bde99a6beeb2907dc757a042c4
Trevor Liberty <tliberty@pdx.edu>
2021-04-17 18:52:27 -0700

    UI additions, . Co-authored-by: Joe Leland <joe@stackoverjoe.com>

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-	res.render('index');
+	res.render('index', { articles: objects });
 });
 
 io.on('connection', (socket) => {
