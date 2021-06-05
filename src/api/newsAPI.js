3e53c5d05f667e1adbc40ec48ed53566219288e4
Joe Leland <jml27@pdx.edu>
2021-06-05 16:50:00 -0700

    API key was visible in code.. eesh.. move to env file and add brief comments to functions

--- src/api/newsAPI.js
+++ src/api/newsAPI.js
@@ -1,54 +1,50 @@
-require('dotenv').config();
-const { v4: uuidv4 } = require('uuid');
-const fetch = require('node-fetch');
-const NewsAPI = require('newsapi');
+require("dotenv").config();
+const fetch = require("node-fetch");
+const NewsAPI = require("newsapi");
 
 const newsapi = new NewsAPI(process.env.API_KEY);
-const diffbotkey = process.env.DIFFBOT_TOKEN;
+const rapidApiKey = process.env.DIFFBOT_TOKEN;
 
-const rapidapikey = '3d9acc06dbmshfc5ce11c24971b0p116d65jsn008041f167b2';
-
+//API is used to get the contents of an article as html for prenestation in TopTalk
 const getHTML = async (url) => {
-	let response = await fetch(
-		`https://diffbot-diffbot.p.rapidapi.com/v2/article?token=9216af864a99daaa340cf5bba2ddfc5c&url=${url}&timeout=15000&fields=html`,
-		{
-			method: 'GET',
-			headers: {
-				'x-rapidapi-key': '3d9acc06dbmshfc5ce11c24971b0p116d65jsn008041f167b2',
-				'x-rapidapi-host': 'diffbot-diffbot.p.rapidapi.com',
-			},
-		},
-	)
-	let json = await response.json()
+  let response = await fetch(
+    `https://diffbot-diffbot.p.rapidapi.com/v2/article?token=9216af864a99daaa340cf5bba2ddfc5c&url=${url}&timeout=15000&fields=html`,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": rapidApiKey,
+        "x-rapidapi-host": "diffbot-diffbot.p.rapidapi.com",
+      },
+    }
+  );
+  let json = await response.json();
 
-	return json.html
+  return json.html;
 };
+
+//Get todays top heeadlines from the api, only stores in the us
 const getTopHeadlines = async () => {
-	let response = await newsapi.v2.topHeadlines({
-		country: 'us',
-	});
+  let response = await newsapi.v2.topHeadlines({
+    country: "us",
+  });
 
-	return response['articles'];
+  return response["articles"];
 };
+
+//Function to find related articles given a title
 const getQueryHeadlines = async (title, url) => {
-	let response = await newsapi.v2.everything({
-		q: title,
-	});
+  let response = await newsapi.v2.everything({
+    q: title,
+  });
 
-	return (response = response['articles'].filter((article) => {
-		return article.source.name !== 'Slashdot.org' && url !== article.url;
-	}));
-
-	//  return response.map((article) => {
-	//  	let html = getHTML(article.url);
-	// 	 console.log(html);
-	//  	article['htmlBody'] = html
-	//  	return article
-	//  });
+  return (response = response["articles"].filter((article) => {
+    return article.source.name !== "Slashdot.org" && url !== article.url;
+  }));
 };
 
+//Export functions to be used elsewhere in the app
 module.exports = {
-	getTopHeadlines,
-	getQueryHeadlines,
-	getHTML
+  getTopHeadlines,
+  getQueryHeadlines,
+  getHTML,
 };
