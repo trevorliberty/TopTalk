d9b943ab572da703b656679fe4a24532633fd881
Joe Leland <jml27@pdx.edu>
2021-06-06 09:44:10 -0700

    Data process has lots of complex logic and no documentation. Write detailed comments explaining funcionality. Remove debug console logs

--- src/api/dataprocess.js
+++ src/api/dataprocess.js
@@ -4,12 +4,17 @@ const { v4: uuidv4 } = require("uuid");
 
 const { getTopHeadlines, getQueryHeadlines, getHTML } = require("./newsAPI");
 
+//Function to filter through the words of an article title and
+//remove non meanigful words so they are not considered when trying
+//to find related articles
 const getWords = (theString) => {
   theString = theString.toLowerCase().trim();
   stringArr = theString.split(" ");
   return stringArr.filter((word) => !blackList[word]);
 };
 
+//Set all of the original titles words in a map to be referenced
+//for weight processing
 const setWordMapInfo = (title, WordMap) => {
   const words = getWords(title);
   words.forEach((word) => {
@@ -17,11 +22,15 @@ const setWordMapInfo = (title, WordMap) => {
   });
 };
 
+//News api always prepends titles with "Source - "
+//We want to remove this from all titles
 const titleFormatter = (title) => {
   let idx = title.lastIndexOf("-");
   return title.substring(0, idx).trim();
 };
 
+//Here we can augment the results with a weight value based
+//on the contents of the WordMap
 const processWeights = (results, WordMap) => {
   results.forEach((result) => {
     let weight = 0;
@@ -32,23 +41,31 @@ const processWeights = (results, WordMap) => {
         weight += WordMap.get(title);
       }
     });
+    //New object value named 'weight' created here
     result["weight"] = weight;
   });
   return results;
 };
+
+//Function is called on every articles that is queried with the source title
+//as the query parameter. A lot of these are not actually related so we go through
+//the title and process word occurences against the original and set the WordMap accordingly
 const processWordMap = (results, WordMap) => {
   results.forEach((result) => {
     let title = result["title"];
     title = getWords(title);
     title.forEach((word) => {
       if (WordMap.has(word)) {
-        let scala = WordMap.get(word);
-        WordMap.set(word, ++scala);
+        let wordWeightValue = WordMap.get(word);
+        WordMap.set(word, ++wordWeightValue);
       }
     });
   });
 };
 
+//The article at results[0] is the source article and by default sets the weight standard
+//The results are filtered over and if they meet a threshold of relevance, tuend through trial and error,
+//they are included as a related article
 const determineSelections = (results) => {
   if (results[0]) {
     let highestWeight = results[0].weight;
@@ -56,10 +73,12 @@ const determineSelections = (results) => {
       return result.weight > 0.45 * highestWeight;
     });
   } else {
-    console.log("oops");
+    console.log("No results were found.");
   }
 };
 
+//Async funciton that orchestrates the daily retrieval of articles and
+//processes the results in a way that the UI knows how to interpret
 module.exports = async () => {
   let objects = [];
   let articles = await getTopHeadlines();
@@ -85,14 +104,14 @@ module.exports = async () => {
 
     if (results) {
       results = await postProcess(results);
+      //When all the promises have resolved construct the object and return
       return Promise.all(results).then((results) => {
-        console.log(results);
-        const obj = {
+        const mainArticleObject = {
           id: uuidv4(),
           source: article,
           relatedArticles: results,
         };
-        return obj;
+        return mainArticleObject;
       });
     }
   });
@@ -106,16 +125,20 @@ module.exports = async () => {
     });
     values.forEach((article) => {
       article.relatedArticles.map((relatedArticle) => {
+        //Each article is given a unique ID as the UI will use this value for event listeners and duplicates will break
+        //all website functionality
         relatedArticle["id"] = uuidv4();
         return relatedArticle;
       });
     });
 
+    //Minimize api calls for developing by caching results in a file
     fs.writeFile("results.json", JSON.stringify(values, null, 4), (e) => {});
     return values;
   });
 };
 
+//Function remove huge video elements from article html
 const postProcess = async (relatedArticles) => {
   return relatedArticles.map(async (relatedArticle) => {
     let html = await getHTML(relatedArticle.url);
